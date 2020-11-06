87eb841907b6af30e74e137e7189012c316c4826
iharicharan <xprogrammer.me@gmail.com>
2020-11-06 13:48:38 +0530

    Updates

---
+++ precache-manifest.92a1cdf57028c343d996b1be3013d720.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e30b44de933a4199b3daf15e0dc6609f",
+    "url": "/MagicLink/index.html"
+  },
+  {
+    "revision": "1d8aecf9d59c6fc3f5ae",
+    "url": "/MagicLink/static/css/main.4897d5f1.chunk.css"
+  },
+  {
+    "revision": "543e68494adb58cb54c4",
+    "url": "/MagicLink/static/js/2.94eb7497.chunk.js"
+  },
+  {
+    "revision": "78fd0f62364854e39c53711d027f73a3",
+    "url": "/MagicLink/static/js/2.94eb7497.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d8aecf9d59c6fc3f5ae",
+    "url": "/MagicLink/static/js/main.a4df8c79.chunk.js"
+  },
+  {
+    "revision": "f98ddb7b69c84df1fd5d",
+    "url": "/MagicLink/static/js/runtime-main.7a123b25.js"
+  },
+  {
+    "revision": "c41d7f3039c72d7ea83733fd7939f923",
+    "url": "/MagicLink/static/media/demo.c41d7f30.mp3"
+  },
+  {
+    "revision": "216bcf59f9c481ca06e0865719ab4d45",
+    "url": "/MagicLink/static/media/ram.216bcf59.gif"
+  }
+]);
